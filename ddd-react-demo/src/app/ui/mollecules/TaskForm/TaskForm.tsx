26f026977f00f4a676f94815df06b7836f8c2007
tictools <jaumevillarreal@gmail.com>
2025-03-16 12:26:55 +0100

    feat: Extract useCreateTaskForm hook

--- ddd-react-demo/src/app/ui/mollecules/TaskForm/TaskForm.tsx
+++ ddd-react-demo/src/app/ui/mollecules/TaskForm/TaskForm.tsx
@@ -1,32 +1,10 @@
-import { PartialTaskValues } from "../../../../core/Task/domain/Task/TaskValues";
-import { type TaskStatus } from "../../../../core/Task/domain/valueObjects/Status";
-import { useCreateTaskCase } from "../../../api/Task/mutations/useCreateTaskCase";
 import { HeadingTertiary } from "../../atoms/HeadingTertiary/HeadingTertiary";
+import { useCreateTaskForm } from "./hooks/useCreateTaskForm";
 
 import styles from "./TaskForm.module.css";
 
 export const TaskForm = () => {
-  const { mutate: executeCreateTaskUseCase, error } = useCreateTaskCase();
-
-  const handleCreateNewTaskUseCase = (
-    event: React.FormEvent<HTMLFormElement>
-  ) => {
-    event.preventDefault();
-
-    const formData = new FormData(event.target as HTMLFormElement);
-
-    const partialNewTask: PartialTaskValues = {
-      title: formData.get("title") as string,
-      description: formData.get("description") as string,
-      status: formData.get("status") as TaskStatus,
-      dueDate: new Date(formData.get("dueDate") as string).getTime(),
-      userUUID: "1ee9333c-4133-4b1b-871a-fd8dc223eb24", // Assuming userUUID is set elsewhere
-    };
-
-    executeCreateTaskUseCase(partialNewTask);
-
-    (event.target as HTMLFormElement).reset();
-  };
+  const { handleCreateNewTaskUseCase, error } = useCreateTaskForm();
 
   return (
     <div className={styles["container"]}>
@@ -63,7 +41,7 @@ export const TaskForm = () => {
           </label>
           <select name="status" id="status" className={styles["select"]}>
             <option value="pending">Pending</option>
-            <option value="inProgress">In Progress</option>
+            <option value="in-progress">In Progress</option>
             <option value="done">Done</option>
           </select>
         </div>
---
+++ ddd-react-demo/src/app/ui/mollecules/TaskForm/hooks/useCreateTaskForm.ts
@@ -0,0 +1,32 @@
+import { type PartialTaskValues } from "../../../../../core/Task/domain/Task/TaskValues";
+import { type TaskStatus } from "../../../../../core/Task/domain/valueObjects/Status";
+import { useCreateTaskCase } from "../../../../api/Task/mutations/useCreateTaskCase";
+
+export const useCreateTaskForm = () => {
+  const { mutate: executeCreateTaskUseCase, error } = useCreateTaskCase();
+
+  const handleCreateNewTaskUseCase = (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target as HTMLFormElement);
+
+    const partialNewTask: PartialTaskValues = {
+      title: formData.get("title") as string,
+      description: formData.get("description") as string,
+      status: formData.get("status") as TaskStatus,
+      dueDate: new Date(formData.get("dueDate") as string).getTime(),
+      userUUID: "1ee9333c-4133-4b1b-871a-fd8dc223eb24", // Assuming userUUID is set elsewhere
+    };
+
+    executeCreateTaskUseCase(partialNewTask);
+
+    (event.target as HTMLFormElement).reset();
+  };
+
+  return {
+    handleCreateNewTaskUseCase,
+    error,
+  };
+};
